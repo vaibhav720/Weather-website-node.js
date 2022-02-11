e62d9a1b45d481e4d5c855cf38f79212349eff6b
Vaibhav Dipesh Parikh <56918464+vaibhav720@users.noreply.github.com>
2022-02-11 23:58:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const https = require("https");
+const app = express();
+const bodyParser= require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+    
+});
+
+app.post("/",function(req,res){
+    
+    const query= req.body.cityName;
+    const apiKey = "8c9ddbd887c66edb6acf58ad752f9b96";
+    const unit="metric";
+   const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit;
+
+    https.get(url,function(response){
+        
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp =weatherData.main.temp;
+            const weatherdescription =weatherData.weather[0].description;
+            
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+            
+            console.log(temp);
+            console.log(imageURL);
+           
+            res.write("<h1>The temperature in "+query+" is "+temp+" and cloud is "+weatherdescription+"</h1>");
+            res.write("<img src='"+imageURL+"'>");
+            res.send();
+        });
+    })
+});
+
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
